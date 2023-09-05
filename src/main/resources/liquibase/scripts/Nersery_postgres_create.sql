c001941674e7a8761c502a1cf1e36687a51a8291
RHONT <rg915i@yandex.ru>
2023-09-05 23:06:17 +0300

    + комментировал некоторые поля в скрипте по созданию таблиц + скорректировал поля таблиц

--- src/main/resources/liquibase/scripts/Nersery_postgres_create.sql
+++ src/main/resources/liquibase/scripts/Nersery_postgres_create.sql
@@ -1,9 +1,11 @@
 -- liquibase formatted sql
 -- changeset Evgeniy:1
 
-
 CREATE TABLE "visitors" (
+-- чат пользователя
 	"chat_id" bigint NOT NULL,
+-- 	заносим номер питомника, переменная нужна для быстрого определения с каким питомником мы будем работать.
+	"number_nursary" int,
 	CONSTRAINT "visitors_pk" PRIMARY KEY ("chat_id")
 );
 
@@ -15,10 +17,11 @@ CREATE TABLE "person" (
 	CONSTRAINT "person_pk" PRIMARY KEY ("id_person")
 );
 
+
 CREATE TABLE "report" (
 	"id_report" serial NOT NULL,
 	"id_person" bigint,
-	"forteit" bigint default '0',
+	"forteit" bigint default '0', -- 	Штраф за плохое заполнения отчета.
 	"day_report" bigint DEFAULT '30',
 	CONSTRAINT "report_pk" PRIMARY KEY ("id_report")
 );
@@ -27,9 +30,10 @@ CREATE TABLE "nursary" (
 	"id_nursary" serial NOT NULL,
 	"name_nursary" TEXT,
 	"about" TEXT,
-	"infrastructure" TEXT,
-	"accident_prevention" TEXT,
-	"list_document" TEXT,
+	"infrastructure" TEXT,      -- Все что касается заезда, проезда, охраны и т.д.
+ 	"accident_prevention" TEXT, -- правила поведения на территории
+	"how_get_pet" TEXT,         -- Процедура, как взять животное из этого приюта
+    "list_document" TEXT,
 	CONSTRAINT "nursary_pk" PRIMARY KEY ("id_nursary")
 );
 
@@ -37,6 +41,7 @@ CREATE TABLE "pet" (
 	"id_nursary" bigint NOT NULL,
 	"id_pet" serial NOT NULL,
 	"nickname" varchar(50),
+	"age" int,
 	"foto" bytea,
 	"diet" TEXT,
 	"dating_rule" TEXT,
@@ -44,7 +49,8 @@ CREATE TABLE "pet" (
 	"house_recomend" TEXT,
 	"cynologist_advice" TEXT,
 	"cynologist_advice_up" TEXT,
-	"reasons_refusal" TEXT,
+	"reasons_refusal" TEXT,  -- почему нельзя взять
+	"invalid" bool default 'false',
 	"person_id" bigint,
 	CONSTRAINT "pet_pk" PRIMARY KEY ("id_pet")
 );
@@ -55,8 +61,14 @@ CREATE TABLE "data_report" (
 	"foto" bytea,
 	"diet" TEXT,
 	"health" TEXT,
-	"demeanor" TEXT,
+	"demeanor" TEXT,  -- поведение животного
 	CONSTRAINT "data_report_pk" PRIMARY KEY ("date_datas")
+);
+
+CREATE TABLE  "hous_recomend_varian"(
+    "type_animal" varchar(15),
+    "age" int,
+    "description" text
 );
 
 ALTER TABLE "report" ADD CONSTRAINT "report_fk0" FOREIGN KEY ("id_person") REFERENCES "person"("id_person");
