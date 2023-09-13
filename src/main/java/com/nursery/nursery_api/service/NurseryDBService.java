690ab0996294a8039605f812b65e76d5cf9aa33b
RHONT <rg915i@yandex.ru>
2023-09-13 15:34:50 +0300

    + Решил для связи созадть ConnectService, чтобы не перегружать сервис NurseryDBService

--- src/main/java/com/nursery/nursery_api/service/NurseryDBService.java
+++ src/main/java/com/nursery/nursery_api/service/NurseryDBService.java
@@ -26,25 +26,13 @@ public class NurseryDBService {
      */
     private final Map<String, Nursery> nurseryMap=new HashMap<>();
 
-    /**
-     * key - Volunteer
-     * value - chat id human needy
-     */
-    private final Map<Volunteer,Long> volunteersList =new ConcurrentHashMap<>();
-
-    /**
-     * contain all chat id (volunteer and human needy)
-     */
-    private final Set<Long> dialogs=new ConcurrentSkipListSet<>();
-
-
     private final DataReportRepository dataReportRepository;
     private final NurseryRepository nurseryRepository;
     private final PersonRepository personRepository;
     private final PetRepository petRepository;
     private final ReportRepository reportRepository;
     private final VisitorsRepository visitorsRepository;
-    private final VolunteerRepository volunteerRepository;
+
 
     public NurseryDBService(DataReportRepository dataReportRepository, NurseryRepository nurseryRepository, PersonRepository personRepository, PetRepository petRepository, ReportRepository reportRepository, VisitorsRepository visitorsRepository, VolunteerRepository volunteerRepository) {
         this.dataReportRepository = dataReportRepository;
@@ -53,7 +41,7 @@ public class NurseryDBService {
         this.petRepository = petRepository;
         this.reportRepository = reportRepository;
         this.visitorsRepository = visitorsRepository;
-        this.volunteerRepository = volunteerRepository;
+
     }
 
     // Заводим для кэша мапу с посетителями. Она скудная, лишний раз не будем дергать БД,
@@ -64,13 +52,6 @@ public class NurseryDBService {
             for (var element:listFromDBVisitors) {
                 visitors.put(element.getChatId(),element.getNameNursery());
             }
-        }
-
-        // Записываем из базы всех волонтеров в оперативную память
-        // Так как у них нет пока собеседника ставим в значение null
-        List<Volunteer> allVolunteers = volunteerRepository.findAll();
-        for (var element:allVolunteers) {
-            volunteersList.put(element,null);
         }
     }
 
@@ -189,100 +170,7 @@ public class NurseryDBService {
         return nurseryMap.get(visitors.get(idChat)).getReasonsRefusal();
     }
 
-    // связываем свободного волонтера с страждущим человеком
-    public void addToDialogsUser(Long chatId){
-        Optional<Volunteer> volunteer = volunteersList.keySet().stream().filter(e -> !e.isBusy()).findFirst();
-        if (volunteer.isPresent()) {
-            volunteer.get().setBusy(true);
-            volunteersList.put(volunteer.get(),chatId);
-            dialogs.add(chatId);
-            dialogs.add(volunteer.get().getVolunteerChatId());
-        }
-    }
 
-    // волонтер пишет "-к", а значит беседу нужно закончить
-    public void disconnect(Long chatIdVolunteer){
-        Optional<Volunteer> volunteer=getVolunteerByChatId(chatIdVolunteer);
-        if (volunteer.isPresent()) {
-            // удаляем из диалогов и волонтера и вопрошающего
-            dialogs.remove(volunteersList.get(volunteer.get()));
-            dialogs.remove(volunteer.get().getVolunteerChatId());
-            // освобождаем волонтера
-            volunteer.get().setBusy(false);
-        } else throw new NoSuchElementException("В журнале такого волонтера нет!");
-    }
-
-    // Если волонтер заканчивает свою смену
-    // он пишет в чат бот "Стоп работа"
-    public void disappearanceVolunteer(Long chatIdVolunteer){
-        // делаем проверку нет ли волонтера в активной беседе
-        // если есть, то чистим ее
-        if (dialogs.contains(chatIdVolunteer)) {
-            disconnect(chatIdVolunteer);
-        }
-        // ставим волонтеру занятость на true
-        Optional<Volunteer> volunteer= volunteersList.keySet().stream().filter(e-> Objects.equals(e.getVolunteerChatId(), chatIdVolunteer)).findFirst();
-        volunteer.ifPresent(volunteerFree-> volunteerFree.setBusy(true));
-    }
-
-    // Добавляем в работу нового волонтера
-    // пока не принял решение как он будет туда заноситься
-    // Пусть будет "- Хочу стать волонтером: @GonzaMy"
-    // Нужно прописать проверку в телеграмме на строку формата message.starWith("- Хочу стать волонтером:")
-    // Регуляркой достать @GonzaMy и сохранить в базу нового пользователя
-
-    public Volunteer addNewVolunteer(Volunteer volunteer){
-        if (volunteer!=null) {
-            // заносим товарища в базу
-            volunteerRepository.save(volunteer);
-            return volunteer;
-
-        } else throw new NoSuchElementException("Волонтер = null!");
-    }
-
-    // Если волонтер хочет самовыпилиться
-    // например пишет в чат "- Не хочу быть волонтером @GonzaMy"
-    // Нужно прописать проверку в телеграмме на строку формата message.starWith("- Не хочу быть волонтером")
-    // и вытащить от туда @GonzaMy
-    public Volunteer hasLeftVolunteer(String telegramName){
-        Optional<Volunteer> volunteer=volunteerRepository.findByTelegramName(telegramName);
-        if (volunteer.isPresent()) {
-            volunteerRepository.deleteByTelegramName(telegramName);
-            // чистим общий чат
-            disconnect(volunteer.get().getVolunteerChatId());
-            // удаляем из мапы
-            volunteersList.remove(volunteer.get());
-        } else throw new NoSuchElementException("Волонтера с таким именем нет!");
-
-        return volunteer.get();
-    }
-
-    // Когда волонтер выходит на смену, идем базу достаем от туда волонтера и добавляем в оперативную память
-    // Но  если он уже есть в оперативной памяти, то просто ставим занятость на false
-    // пусть он вводит: "Волонтер работа: @GonzaMe"
-    // если нет возвращаем null
-    public Volunteer goOnShiftVolunteer(Long chatIdVolunteer){
-        Optional<Volunteer> volunteer= volunteersList.keySet().stream().filter(e-> Objects.equals(e.getVolunteerChatId(), chatIdVolunteer)).findFirst();
-        if (volunteer.isPresent()) {
-            volunteer.get().setBusy(false);
-            return volunteer.get();
-        } else {
-            Optional<Volunteer> volunteerFromDB=volunteerRepository.findByVolunteerChatId(chatIdVolunteer);
-            if (volunteerFromDB.isPresent()) {
-                volunteerFromDB.get().setBusy(false);
-                volunteersList.put(volunteerFromDB.get(),null);
-                return volunteerFromDB.get();
-            } else throw new NoSuchElementException("Вас нет в базе данных! Обратитесь к администратору");
-        }
-    }
-
-    private Optional<Volunteer> getVolunteerByChatId(Long chatIdVolunteer){
-        return volunteersList.
-                keySet().
-                stream().
-                filter(e-> Objects.equals(e.getVolunteerChatId(), chatIdVolunteer)).
-                findFirst();
-    }
 
 
 
