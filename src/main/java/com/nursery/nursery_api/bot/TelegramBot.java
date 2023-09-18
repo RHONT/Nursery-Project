21efdd1cbfb282b481d84262ed4bb26639118e97
margo <margo171190@mail.ru>
2023-09-18 23:15:54 +0400

    Рефакторанг TelegramBot

--- src/main/java/com/nursery/nursery_api/bot/TelegramBot.java
+++ src/main/java/com/nursery/nursery_api/bot/TelegramBot.java
@@ -62,113 +62,29 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
 
-//    @SneakyThrows
+    //    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
 
-        if (update.hasMessage()) {
+        if (update.hasMessage() && update.getMessage().hasPhoto()) {
             Message message = update.getMessage();
             Chat chat = message.getChat();
-// проверяем отчет, если есть фото, значит это отчет
-            // нужно сгененировать это событие на нажатие кнопки прислать отчет
+            // проверяем отчет, если есть фото, значит это отчет
             if (message.hasPhoto()) {
-                Optional<DataReport> dataReport = dataReportRepository.findDataReportByIdChatAndDateNow(chat.getId(),LocalDate.now());
-                if (dataReport.isPresent()) {
-                    // пишем логику
-                    DataReport dataReport1=dataReport.get();
-
-                    List<PhotoSize> photos = message.getPhoto();
-                    PhotoSize photo = photos.get(photos.size() - 1);
-
-                    String messageCaption=update.getMessage().getCaption();
-
-                    GetFile getFile = new GetFile();
-                    getFile.setFileId(photo.getFileId());
-
-                    File file = null;
-
-                    try {
-                        file = this.execute(getFile);
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-
-
-                    try( InputStream is = new URL("https://api.telegram.org/file/bot" + getBotToken() + "/" + file.getFilePath()).openStream();)
-                    {
-                        dataReport1.setFoto(is.readAllBytes());
-                        dataReport1.setDateReport(LocalDate.now());
-                        dataReport1.setMessagePerson(messageCaption);
-                        dataReport1.setFileSize(file.getFileSize());
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    dataReportRepository.save(dataReport1);
+               saveToDB(chat, message,update);
 
                 } else {
-                   sendSimpleText(chat.getId(), "Возможно вы ошиблись");
+                    sendSimpleText(chat.getId(), "Возможно вы ошиблись");
                 }
-
-
-                // new DataReport() = делал для теста, а нужно
-                // Тут нужно написать запрос, чтобы вернулся отчет где стоит сегодняшняя дата
-                // по шедулеру каждую ночь будет создаваться пустая запись у каждого, кто проходит испытания
-                // То есть нужно найти человека, который есть в базе, найти отчет где он фигурирует и вернуть день этого отчета
-                // Так как мы храним в таблице visitors номер питомника, как результат, мы можем точно найти эту запись
-                // в таблице Person теперь есть  поле id_nursery
-
-
-
-                // тут для теста я взял готовую запись у себя
-                // в репозитории нужно написать методы:
-                // Найти отчет для человека, у человека есть chat_Id
-                // удобнее это будет сделать через @Query
-
-
             }
-        }
-
-        // Дальше идет старый рабочий код!
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             if (!update.getMessage().getText().isEmpty()) {
                 String message = "-main";
                 Long chatIdUser = update.getMessage().getChatId();
 
-                if (connectService.containInActiveDialog(chatIdUser)) {   // является ли chat id участником активной беседы?
-                    if (!connectService.isPerson(chatIdUser)) {           // chat id - это волонтер?
-                        String checkedMessage = update.getMessage().getText();
-                        checkVolunteerOperation(checkedMessage, chatIdUser);
-                        return;
-                    }
+                communicationWithVolunteer(chatIdUser, update, message);
 
-                    if (connectService.isPerson(chatIdUser)) {    // если chat id вопрошающий, то шлем сообщение волонтеру
-                        sendSimpleText(connectService.getVolunteerChatIdByPersonChatId(chatIdUser), update.getMessage().getText());
-
-                    } else {                                        // если нет, то наоборот
-                        sendSimpleText(connectService.getPersonChatIdByChatIdVolunteer(chatIdUser), update.getMessage().getText());
-                    }
-
-                } else {
-
-                    // если пользователь впервые
-                    if (!nurseryDBService.contain(chatIdUser)) {
-                        sendSimpleText(update.getMessage().getChatId(), "Здравствуйте, это питомник домашних животных!");
-                    }
-                    checkMessage(message, chatIdUser);       // При любой непонятной команде выводим главное меню чата
-                }
-                // Регистрация волонтера. Тут же id_chat попадает в базу
-                if (update.getMessage().getText().startsWith("Хочу стать волонтером")) {
-                    Volunteer volunteer = new Volunteer();
-                    volunteer.setBusy(false);
-                    volunteer.setVolunteerChatId(chatIdUser);
-                    connectService.addNewVolunteer(volunteer);
-                }
-                if (update.getMessage().getText().startsWith("Я ухожу")) {
-                    connectService.iAmGonnaWayVolunteer(chatIdUser);
-                }
             }
             // проверяем ответы от кнопок
         } else if (update.hasCallbackQuery()) {
@@ -177,6 +93,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             checkMessage(message, idChat);
         }
     }
+
 
     @Override
     public String getBotUsername() {
@@ -237,13 +154,94 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
 
     }
+
+    /**
+     * сохраняем фото и сообщение из caption отдельно в БД
+     * @param chat
+     * @param message
+     * @param update
+     */
+    private void saveToDB(Chat chat, Message message, Update update) {
+        Optional<DataReport> dataReport = dataReportRepository.findDataReportByIdChatAndDateNow(chat.getId(), LocalDate.now());
+        if (dataReport.isPresent()) {
+            DataReport dataReport1 = dataReport.get();
+
+            List<PhotoSize> photos = message.getPhoto();
+            PhotoSize photo = photos.get(photos.size() - 1);
+
+            String messageCaption = update.getMessage().getCaption();
+
+            GetFile getFile = new GetFile();
+            getFile.setFileId(photo.getFileId());
+
+            File file = null;
+
+            try {
+                file = this.execute(getFile);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+
+            try (InputStream is = new URL("https://api.telegram.org/file/bot" + getBotToken() + "/" + file.getFilePath()).openStream();) {
+                dataReport1.setFoto(is.readAllBytes());
+                dataReport1.setDateReport(LocalDate.now());
+                dataReport1.setMessagePerson(messageCaption);
+                dataReport1.setFileSize(file.getFileSize());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            dataReportRepository.save(dataReport1);
+        }
+    }
+
+    /**
+     * общение с волонтером
+     * @param chatIdUser
+     * @param update
+     * @param message
+     */
+    private void communicationWithVolunteer(long chatIdUser, Update update, String message){
+        if (connectService.containInActiveDialog(chatIdUser)) {   // является ли chat id участником активной беседы?
+            if (!connectService.isPerson(chatIdUser)) {           // chat id - это волонтер?
+                String checkedMessage = update.getMessage().getText();
+                checkVolunteerOperation(checkedMessage, chatIdUser);
+                return;
+            }
+
+            if (connectService.isPerson(chatIdUser)) {    // если chat id вопрошающий, то шлем сообщение волонтеру
+                sendSimpleText(connectService.getVolunteerChatIdByPersonChatId(chatIdUser), update.getMessage().getText());
+
+            } else {                                        // если нет, то наоборот
+                sendSimpleText(connectService.getPersonChatIdByChatIdVolunteer(chatIdUser), update.getMessage().getText());
+            }
+
+        } else {
+
+            // если пользователь впервые
+            if (!nurseryDBService.contain(chatIdUser)) {
+                sendSimpleText(update.getMessage().getChatId(), "Здравствуйте, это питомник домашних животных!");
+            }
+            checkMessage(message, chatIdUser);       // При любой непонятной команде выводим главное меню чата
+        }
+        // Регистрация волонтера. Тут же id_chat попадает в базу
+        if (update.getMessage().getText().startsWith("Хочу стать волонтером")) {
+            Volunteer volunteer = new Volunteer();
+            volunteer.setBusy(false);
+            volunteer.setVolunteerChatId(chatIdUser);
+            connectService.addNewVolunteer(volunteer);
+        }
+        if (update.getMessage().getText().startsWith("Я ухожу")) {
+            connectService.iAmGonnaWayVolunteer(chatIdUser);
+        }
+    }
 }
 
 
-
-
 // Дальше описана логика как мы достаем картинку из базы
-    // и отправляем нужному пользователю.
+// и отправляем нужному пользователю.
 
 //    SendPhoto sendPhoto = new SendPhoto();
 //
